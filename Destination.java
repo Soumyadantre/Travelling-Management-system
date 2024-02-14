9efff31fe8acdc3ea11ef9f38e3e1335bc493869
Soumya <132812436+Soumyadantre@users.noreply.github.com>
2024-02-14 12:24:36 +0530

    Update Destination.java

--- Destination.java
+++ Destination.java
@@ -1,13 +1,34 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 public class Destination {
     private String name;
     private List<Activity> activities;
 
-    // Constructors, getters, and setters go here
+    public Destination(String name) {
+        this.name = name;
+        this.activities = new ArrayList<>();
+    }
 
     public void addActivity(Activity activity) {
         activities.add(activity);
     }
+
+    // Getters and Setters
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Destination that = (Destination) o;
+        return Objects.equals(name, that.name) &&
+                Objects.equals(activities, that.activities);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, activities);
+    }
 }
+
